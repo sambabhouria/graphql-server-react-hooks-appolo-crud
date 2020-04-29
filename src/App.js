e67101c9794c539f03dd5a22e9d4ace218cce8b6
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-04-29 15:44:39 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Fragment } from "react";
+import AddUserForm from "./client/forms/add-user-form";
+import EditUserForm from "./client/forms/edit-user-form";
+import UserTable from "./client/tables/user-table";
 
-function App() {
+const App = () => {
+  // Data
+  const usersData = [
+    { id: 1, name: "Tania", username: "floppydiskette" },
+    { id: 2, name: "Craig", username: "siliconeidolon" },
+    { id: 3, name: "Ben", username: "benisphere" },
+  ];
+
+  const initialFormState = { id: null, name: "", username: "" };
+
+  // Setting state
+  const [users, setUsers] = useState(usersData);
+  const [currentUser, setCurrentUser] = useState(initialFormState);
+  const [editing, setEditing] = useState(false);
+
+  // CRUD operations
+  const addUser = (user) => {
+    user.id = users.length + 1;
+    setUsers([...users, user]);
+  };
+
+  const deleteUser = (id) => {
+    setEditing(false);
+
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  const updateUser = (id, updatedUser) => {
+    setEditing(false);
+
+    setUsers(users.map((user) => (user.id === id ? updatedUser : user)));
+  };
+
+  const editRow = (user) => {
+    setEditing(true);
+
+    setCurrentUser({ id: user.id, name: user.name, username: user.username });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Create Read Updae Delete</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          {editing ? (
+            <Fragment>
+              <h2>Edit user</h2>
+              <EditUserForm
+                editing={editing}
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
+            </Fragment>
+          ) : (
+            <Fragment>
+              <h2>Add user</h2>
+              <AddUserForm addUser={addUser} />
+            </Fragment>
+          )}
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
